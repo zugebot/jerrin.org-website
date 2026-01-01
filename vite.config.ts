52bd127eb6effd0c71550af0f94a28a8840144b4
Jerrin Shirks <zugebot2000@gmail.com>
2026-01-01 00:47:46 -0800

    fixes to rewrite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/jerrin.org-website/",
+  base: "/",
   build: {
     outDir: "docs",
     emptyOutDir: true,

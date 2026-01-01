80faace49582f521e552749be88c94790a6b5380
Jerrin Shirks <zugebot2000@gmail.com>
2026-01-01 00:50:39 -0800

    fixes to rewrite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from "@tailwindcss/vite";
-
+// hi
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],

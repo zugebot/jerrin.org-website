63a13be731b8c87f0aa79c9e6d59a34a4931aef3
Jerrin Shirks <zugebot2000@gmail.com>
2026-04-01 17:05:55 -0700

    I hate vite.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
-// hi
-// https://vite.dev/config/
+import { resolve } from "node:path";
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/",
   build: {
     outDir: "dist",
     emptyOutDir: true,
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        mudae: resolve(__dirname, "mudae/index.html"),
+      },
+    },
   },
 });
